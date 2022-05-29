f6ca3c4fe2de5b41be375e5fc2e42d6c5f5ba1d4
Andrew Circelli <andrewcircelli@gmail.com>
2022-05-29 14:12:58 -0400

    set up multiple functions to handle two queries and one crud, nested implementation for now

--- services/assign-leads-to-campaign.js
+++ services/assign-leads-to-campaign.js
@@ -9,32 +9,58 @@ async function returnLocalLeads() {
   let leads = await jsonModule();
   return leads;
 }
-const queryLead = (req, res, next) =>
+// query SF Lead table based on searchId provided via returnLocalLeads() and call queryCampaign on success
+const queryLead = (req, res) => {
   conn
-    .query(`SELECT Id, Name FROM Lead WHERE id = '${req.searchId}'`)
+    .query(
+      `SELECT Id, Name, ProductInterest__c FROM Lead WHERE id = '${req.searchId}'`
+    )
     .on("record", (leadRecord) => {
-      res.send(leadRecord);
-      next();
+      queryCampaign(req, res, leadRecord);
     })
     .on("error", (err) => {
       res.status(404).send(err);
     });
+};
+
+// query SF Campaign table for applicable campaign record and call createCampaignMember on success
+const queryCampaign = (req, res, leadRecord) => {
+  conn
+    .query(
+      `SELECT Id, Name FROM Campaign WHERE Name = '${leadRecord.ProductInterest__c}'`
+    )
+    .on("record", (campaignRecord) => {
+      createCampaignMember(req, res, campaignRecord);
+    })
+    .on("error", (err) => {
+      res.status(404).send(err);
+    });
+};
+
+// create new CampaignMember based on provided LeadId and identified CampaignId
+const createCampaignMember = (req, res, campaignRecord) => {
+  conn
+    .sobject("CampaignMember")
+    .create(
+      { CampaignId: campaignRecord.Id, LeadId: req.searchId },
+      function (err, ret) {
+        if (err || !ret.success) {
+          // return error response if campaign member is already established
+          return res.status(404).send(err.message);
+        }
+        // send CampaignMemberId back to client
+        res.send(
+          `<h1> Lead ${req.searchId} assocaited to Campaign ${campaignRecord.Id} via CampaignMember ${ret.id}</h1>`
+        );
+      }
+    );
+};
 
 module.exports = function (app) {
-  app.get("/api/assign-leads", async (req, res, next) => {
+  app.get("/api/assign-leads", async (req, res) => {
     const leads = await returnLocalLeads();
     req.searchId = leads.records[0].Id;
     conn = await connectionCheck();
-    queryLead(req, res, next);
+    queryLead(req, res);
   });
 };
-
-// conn.search(
-//   "FIND {Un*} IN ALL FIELDS RETURNING Account(Id, Name), Lead(Id, Name)",
-//   function (err, res) {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     console.log(res);
-//   }
-// );
